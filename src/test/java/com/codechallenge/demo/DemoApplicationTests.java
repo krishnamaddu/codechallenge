97935b0df710486563dc304c3041f1bf5356d006
Venkata Krishna Rao Maddu <krishna.maddu@gmail.com>
2020-07-31 01:23:12 -0400

    master: Fixing the build failure

--- src/test/java/com/codechallenge/demo/DemoApplicationTests.java
+++ src/test/java/com/codechallenge/demo/DemoApplicationTests.java
@@ -3,7 +3,7 @@ package com.codechallenge.demo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
